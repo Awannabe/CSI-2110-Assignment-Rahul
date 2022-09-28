db441b1cd6ccd1bdb575f1648b393be85a78342e
Akram <atassiakram@gmail.com>
2022-09-27 22:11:43 -0400

    Added File

---
+++ PizzeriaQueue.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class PizzeriaQueue{
+
+    Queue<Integer> PizzaBatch=new LinkedList<>();
+
+
+
+
+    //create a new queue every time u have a new pizza type
+    //reuse a queue if u already have that pizza type
+
+    addPizza(){
+        //check does this pizza type already exist
+        //locate it 
+        //open it to start adding to it
+        
+
+
+
+        //create a new 
+    }
+    }
+}
